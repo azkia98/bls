f22433d62b28e952ba94ba0494dc3dba354068a4
Mahdi Ebrahimi <azkia@iran.ir>
2020-03-13 17:46:00 +0330

    create the plans page

---
+++ source/plans.blade.php
@@ -0,0 +1,157 @@
+@extends('_layouts.master')
+
+@section('content')
+
+<!-- Fist Section  -->
+<section class="mt-16 flex justify-center">
+
+    <div class="container">
+
+
+        <div class="text-center mb-10">
+            <h5 class="font-bold">برنامه کلاسی</h5>
+            <p class="text-gray-600">برای مشاهده برنامه ترمی، اسم ترم را تایپ کنید</p>
+        </div>
+
+        <div class="flex justify-center mt-3">
+            <div class="flex bg-gray-100 rounded-lg m-box-shadow-1">
+
+                <button class="text-gray-600 rounded-tr-md rounded-br-md">
+                    <i class="material-icons px-2">
+                        search
+                    </i>
+                </button>
+
+                <input type="text" class="bg-gray-100 outline-none input_search py-2 px-2 rounded-bl-lg rounded-tl-lg"
+                    style="direction: ltr;" placeholder="Search by title">
+
+            </div>
+        </div>
+
+
+    </div>
+
+
+
+</section>
+<!-- End Fist Section  -->
+
+
+<!-- Seconde Section -->
+<section class="mt-12 flex justify-center mb-24 lg:mb-32">
+    <div class="container flex">
+
+        <div class="cards flex flex-wrap justify-center">
+
+            <a href="#"
+                class="cards__card bg-gray-200 rounded-lg shadow-xl flex justify-center items-center hover:text-gray-900 text-gray-600">
+                <b>EG - 1</b>
+            </a>
+
+            <a href="#"
+                class="cards__card bg-gray-200 rounded-lg shadow-xl flex justify-center items-center hover:text-gray-900 text-gray-600">
+                <b>EG - 1</b>
+            </a>
+
+            <a href="#"
+                class="cards__card bg-gray-200 rounded-lg shadow-xl flex justify-center items-center hover:text-gray-900 text-gray-600">
+                <b>EG - 1</b>
+            </a>
+
+            <a href="#"
+                class="cards__card bg-gray-200 rounded-lg shadow-xl flex justify-center items-center hover:text-gray-900 text-gray-600">
+                <b>EG - 1</b>
+            </a>
+
+            <a href="#"
+                class="cards__card bg-gray-200 rounded-lg shadow-xl flex justify-center items-center hover:text-gray-900 text-gray-600">
+                <b>EG - 1</b>
+            </a>
+
+            <a href="#"
+                class="cards__card bg-gray-200 rounded-lg shadow-xl flex justify-center items-center hover:text-gray-900 text-gray-600">
+                <b>EG - 1</b>
+            </a>
+
+            <a href="#"
+                class="cards__card bg-gray-200 rounded-lg shadow-xl flex justify-center items-center hover:text-gray-900 text-gray-600">
+                <b>EG - 1</b>
+            </a>
+
+            <a href="#"
+                class="cards__card bg-gray-200 rounded-lg shadow-xl flex justify-center items-center hover:text-gray-900 text-gray-600">
+                <b>EG - 1</b>
+            </a>
+
+            <a href="#"
+                class="cards__card bg-gray-200 rounded-lg shadow-xl flex justify-center items-center hover:text-gray-900 text-gray-600">
+                <b>EG - 1</b>
+            </a>
+
+            <a href="#"
+                class="cards__card bg-gray-200 rounded-lg shadow-xl flex justify-center items-center hover:text-gray-900 text-gray-600">
+                <b>EG - 1</b>
+            </a>
+
+            <a href="#"
+                class="cards__card bg-gray-200 rounded-lg shadow-xl flex justify-center items-center hover:text-gray-900 text-gray-600">
+                <b>EG - 1</b>
+            </a>
+
+            <a href="#"
+                class="cards__card bg-gray-200 rounded-lg shadow-xl flex justify-center items-center hover:text-gray-900 text-gray-600">
+                <b>EG - 1</b>
+            </a>
+
+            <a href="#"
+                class="cards__card bg-gray-200 rounded-lg shadow-xl flex justify-center items-center hover:text-gray-900 text-gray-600">
+                <b>EG - 1</b>
+            </a>
+
+            <a href="#"
+                class="cards__card bg-gray-200 rounded-lg shadow-xl flex justify-center items-center hover:text-gray-900 text-gray-600">
+                <b>EG - 1</b>
+            </a>
+
+            <a href="#"
+                class="cards__card bg-gray-200 rounded-lg shadow-xl flex justify-center items-center hover:text-gray-900 text-gray-600">
+                <b>EG - 1</b>
+            </a>
+
+            <a href="#"
+                class="cards__card bg-gray-200 rounded-lg shadow-xl flex justify-center items-center hover:text-gray-900 text-gray-600">
+                <b>EG - 1</b>
+            </a>
+
+            <a href="#"
+                class="cards__card bg-gray-200 rounded-lg shadow-xl flex justify-center items-center hover:text-gray-900 text-gray-600">
+                <b>EG - 1</b>
+            </a>
+
+            <a href="#"
+                class="cards__card bg-gray-200 rounded-lg shadow-xl flex justify-center items-center hover:text-gray-900 text-gray-600">
+                <b>EG - 1</b>
+            </a>
+
+            <a href="#"
+                class="cards__card bg-gray-200 rounded-lg shadow-xl flex justify-center items-center hover:text-gray-900 text-gray-600">
+                <b>EG - 1</b>
+            </a>
+
+            <a href="#"
+                class="cards__card bg-gray-200 rounded-lg shadow-xl flex justify-center items-center hover:text-gray-900 text-gray-600">
+                <b>EG - 1</b>
+            </a>
+
+            <a href="#"
+                class="cards__card bg-gray-200 rounded-lg shadow-xl flex justify-center items-center hover:text-gray-900 text-gray-600">
+                <b>EG - 1</b>
+            </a>
+
+        </div>
+
+    </div>
+</section>
+<!-- End Seconde Section -->
+
+@endsection
