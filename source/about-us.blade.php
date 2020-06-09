8fffc1556f63ac772f6d1a0136779ccc818585e3
Mahdi <azkia@iran.ir>
2020-06-09 18:55:27 +0430

    add

--- source/about-us.blade.php
+++ source/about-us.blade.php
@@ -50,3 +50,43 @@
 
 
 @endsection
+
+
+
+@section('scripts')
+<script type="text/javascript" src="https://cdn.map.ir/web-sdk/1.4.2/js/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="https://cdn.map.ir/web-sdk/1.4.2/js/mapp.env.js"></script>
+<script type="text/javascript" src="https://cdn.map.ir/web-sdk/1.4.2/js/mapp.min.js"></script>
+
+<script>
+    $(document).ready(function () {
+        var lat = 34.090134;
+        var lng = 49.692657;
+        var app = new Mapp({
+            element: '#map',
+            presets: {
+                latlng: {
+                    lat: lat,
+                    lng: lng,
+                },
+                zoom: 30,
+            },
+            apiKey: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjgxNTkwMTI0YTAyNjQ5YzEwYjNkZTMzMWQwNGU0OWM1ZThjNzBlM2NhNGFkZjRlYTVlNGFhNDlmZGVjODc1Yjg0Y2VmM2Q2Y2FiMzY1ZDA3In0.eyJhdWQiOiI4MjQ0IiwianRpIjoiODE1OTAxMjRhMDI2NDljMTBiM2RlMzMxZDA0ZTQ5YzVlOGM3MGUzY2E0YWRmNGVhNWU0YWE0OWZkZWM4NzViODRjZWYzZDZjYWIzNjVkMDciLCJpYXQiOjE1ODM3ODU0ODgsIm5iZiI6MTU4Mzc4NTQ4OCwiZXhwIjoxNTg2MjAxMDg4LCJzdWIiOiIiLCJzY29wZXMiOlsiYmFzaWMiXX0.RirVN1CKzM5RViHjX6JgYtZRmu8CzpwT5dx1OVJkR0R1JKYWwdLosqUzr4a3f8G-LppodEZqXoUct1uG6oNbse6Gt9_AUijldlH0j83wHwRz-v_F7mfdXaPe1ji8laRJSOksR092IyZdJol-S9QjlwouW14tvGIys4UvXL6RuVaz2MmfeVG2k-FoSoZD_XUCQ715fWkxm22Q3dK_ey3-tCk3SLILi5W9mGq5MQphIV-r4HLnAYh-g-Tv19zN2DCG3PSUCSgPfKoChWrFz7IfFvmXn-w3xtp-hzK8l8ibIUax2CyKxUb_lOCM210EvcR2WSfdLMeoDxsMJ26a1KPpNw'
+        });
+        app.addLayers();
+
+        app.addMarker({
+            name: 'آموزشگاه بیستون',
+            latlng: {
+                lat: lat,
+                lng: lng,
+            },
+            popup: {
+                open: false,
+            },
+        });
+
+
+    });
+</script>
+@endsection
